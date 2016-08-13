352ee50dff0a51e7b2ab58cd7819985992954c18
Patrick O'Keeffe <pokeeffe@wsu.edu>
2016-08-12 17:14:50 -0800

    Add MQTT reporting to Licor CO2 logging service

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Licor CO2/H2O analyzer
+echo "Installing CO2/H2O analyzer logging service..."
+cp scripts/co2-logger.py /usr/sbin/co2-logger
+chmod +x /usr/sbin/co2-logger
+
+echo "Registering service..."
+cp etc/systemd/system/co2-logger.service /etc/systemd/system/
+
+echo "Enabling start at boot..."
+systemctl enable co2-logger.service
+
+echo "Starting service..."
+systemctl restart co2-logger.service
+
 
 # mass flow control routine
 echo "Installing mass flow controller script..."
@@ -13,15 +27,16 @@ cp scripts/typek-logger.py /usr/sbin/typek-logger
 chmod +x /usr/sbin/typek-logger
 cp etc/tracer/typek-logger.conf /etc/tracer/
 
-echo "Registering..."
+echo "Registering service..."
 cp etc/systemd/system/typek-logger.service /etc/systemd/system/
 
 echo "Enabling start at boot..."
 systemctl enable typek-logger.service
 
-echo "Starting..."
+echo "Starting service..."
 systemctl restart typek-logger.service
 
 
-echo "Finished."
+echo "Performing clean-up..."
+echo "Done."
 
--- scripts/co2-logger.py
+++ scripts/co2-logger.py
@@ -16,6 +16,20 @@ import time
 
 from xml.etree import cElementTree as ET
 
+import paho.mqtt.client as paho
+
+
+########    CONFIG    ######################
+broker_addr = '10.1.1.4'
+broker_port = '1883'
+report_topic = 'home/tracer/li840a/state'
+report_interval = 9.6 # secs
+    # HINT use less than 10 sec to prevent
+    # interval stretching but more than
+    # next lowest record interval (9.5sec)
+############################################
+
+
 from logging.handlers import TimedRotatingFileHandler 
 #                              SocketHandler,
 #                              DEFAULT_TCP_LOGGING_PORT)
@@ -36,19 +50,42 @@ columns = ['co2', 'h2o', 'celltemp', 'cellpres', 'h2odewpoint', 'ivolt']
 units = ['ppmv', 'ppthv', 'degC', 'kPa', 'degC', 'Vdc']
 tsv_names = ['co2', 'h2o', 'T_cell', 'P_cell', 'T_dew', 'pwr_in']
 
-d = dict()
+#### MQTT integration
+client = paho.Client()
+client.connect(broker_addr, broker_port)
+client.loop_start()
+report = ('{{"tstamp": {ts:.2f}, "co2": {co2:.3f}, "h2o": {h2o:.3f}, '
+          '"cell_T": {cell_T:.3f}, "cell_P": {cell_P:.3f}, "dewpoint": '
+          '{dp:.3f}, "pwr_src": {pwr:.3f}}}')
+# HINT initialize report rate limiter flag
+last_report_time = time.time() - report_interval
+
+
 while True:
     try:
-        ## waits up to 1sec for new record
+        d = dict()
+
+        ## HINT waits up to 1sec for new record
         xmlrec = co2port.readline()
-        ## parse XML, skip outer nodes (<li840></li840>)
         datatree = ET.fromstring(xmlrec)[0]
         
         for name in columns:
-            ## lookup values, coerce to float, store in dict
             d[name] = float(datatree.find(name).text)
-        ## save to tab-separated file
+
+        now = time.time()
         tsvlog.info("\t".join([str(d[x]) for x in columns]))
+
+        if (now - last_report_time) > report_interval:
+            last_report_time = now
+            client.publish(report_topic,
+                           report.format(ts=now,
+                                         co2=d['co2'],
+                                         h2o=d['h2o'],
+                                         cell_T=d['celltemp'],
+                                         cell_P=d['cellpres'],
+                                         dp=d['h2odewpoint'],
+                                         pwr=d['ivolt']),
+                           qos=1, retain=True)
 
         #print(json.dumps(d))
         #screen.addstr(0, 0, "Cell T: {0}".format(T_cell))
@@ -57,6 +94,7 @@ while True:
         ## ignore garbled messages (typ. for 1st msg after connect)
         continue
     except (KeyboardInterrupt, SystemExit):
+        client.loop_stop()
         raise
     except:
         pass
